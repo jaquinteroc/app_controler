056ad994f794545a718c9ac4f5bc991d3867a6e3
jaquinteroc <jaqcman1@gmail.com>
2025-03-17 00:13:23 +0100

    Primer Commit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -25,8 +27,16 @@ def contacto(nombre, edad):
     
 def query_string():
     print(request)
+    print(request.args)
+    print(request.args.get('param1'))
+    print(request.args.get('param2'))
     return 'ok'
+
+def pagina_no_encontrada(error):
+    #return render_template('404.html'), 404
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.add_url_rule('/query_string', view_func=query_string)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(debug=True,port=5000)
